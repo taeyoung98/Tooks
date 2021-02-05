047a33d94d41d363d593b3b5179fe898381eeba9
taeyoung <119taeyoung@naver.com>
2021-02-05 17:49:15 +0900

    #1.3 useTabs

---
+++ useTabs.js
@@ -0,0 +1,23 @@
+
+const content = [
+  {
+    key: 1,
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    key: 2,
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
+  }
+];
+  
+export const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) return;
+
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
